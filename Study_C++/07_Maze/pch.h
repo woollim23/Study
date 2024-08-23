9283c28f7d4180bd3ade99feedcaa1312f60a18b
woollim23 <begin2125@gmail.com>
2024-08-23 23:50:31 +0900

    자료구조-맵만들기 진행 중

--- Study_C++/07_Maze/pch.h
+++ Study_C++/07_Maze/pch.h
@@ -5,3 +5,34 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+
+struct Pos
+{
+	bool operator==(Pos& other)
+	{
+		return y == other.y && x == other.x;
+	}
+
+	bool operator!=(Pos& other)
+	{
+		return !(*this == other);
+	}
+
+	Pos operator+(Pos& other)
+	{
+		Pos ret;
+		ret.y = y + other.y;
+		ret.x = x + other.x;
+		return ret;
+	}
+
+	Pos& operator +=(Pos& other)
+	{
+		y += other.y;
+		x += other.x;
+		return *this;
+	}
+
+	int32 y = 0;
+	int32 x = 0;
+};
