fadf903dc1ea8c5e835c56a692c69c24ae3d49a5
woollim23 <begin2125@gmail.com>
2024-08-31 23:49:18 +0900

    <프로젝트 정리>

--- Study_C++/06_Algorithm/Algorithm.cpp
+++ Study_C++/06_Algorithm/Algorithm.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <list>
 using namespace std;
 
 template<typename T>
@@ -83,8 +84,80 @@ private:
 	int _capacity = 0;
 };
 
+template<typename T>
+class Node
+{
+public:
+	Node() : _prev(nullptr), _next(nullptr), _data(T())
+	{
+
+	}
+
+	Node(const T& value) : _prev(nullptr), _next(nulltpr), _data(value)
+	{
+
+	}
+
+private:
+	Node*	 _prev;
+	Node*	 _next;
+	T		 _data;
+};
+
+template<typename T>
+class List
+{
+public:
+	List() : _size(0)
+	{
+		_head = new Node<T>();
+		_tail = new Node<T>();
+		_head->_next = _tail;
+		_tail->_prev = _head;
+	}
+
+	~List()
+	{
+		delete _head;
+		delete _tail;
+	}
+
+	void push_back(const T& value)
+	{
+
+	}
+
+	void pop_back()
+	{
+
+	}
+
+private:
+	Node<T>* AddNode(Node<T>* before, const T& value)
+	{
+		Node<T>* newNode = new Node<T>(value);
+		Node<T>* prevNode = before->_prev;
+
+		prevNode->_next = newNode;
+		newNode->_prev = prevNode;
+
+		newNode->_next = before;
+		before->_prev = newNode;
+
+		_size++;
+
+		return newNode;
+	}
+
+public:
+	Node<T>*	_head;
+	Node<T>*	_tail;
+	int			_size;
+
+};
 int main()
 {
+	/* ------------- 벡터 ---------------
 	vector<int> v;
 
 	// resize를 조정, capacity도 같이 10으로 증가
@@ -106,4 +179,36 @@ int main()
 	v.clear();
 
 	cout << v.size() << " " << v.capacity() << endl;
+
+	*/
+
+
+	list<int> li;
+	
+	list<int>::iterator eraselt;
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (i == 5)
+		{
+			eraselt = li.insert(li.end(), i);
+		}
+		else
+		{
+			li.push_back(i);
+		}
+	}
+	// 리스트는 []를 이용한 임의 접근이 불가능
+
+	li.pop_back();
+
+	li.erase(eraselt);
+
+	for (list<int>::iterator it = li.begin(); it != li.end(); it++)
+	{
+		cout << (*it) << endl;
+	}
+
+
+
 } 
