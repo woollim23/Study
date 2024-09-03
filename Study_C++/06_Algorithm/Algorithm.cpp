d14884b52a9048446d00eb8365c182110c96d63e
woollim23 <begin2125@gmail.com>
2024-09-03 23:16:13 +0900

    [자료구조&알고리즘] 10강. 큐

--- Study_C++/06_Algorithm/Algorithm.cpp
+++ Study_C++/06_Algorithm/Algorithm.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <list>
 #include <stack>
+#include <queue>
 using namespace std;
 
 // 벡터
@@ -302,6 +303,91 @@ private:
 	Container _container;
 };
 
+// 큐
+template<typename T>
+class ArrayQueue
+{
+public:
+	ArrayQueue()
+	{
+		_container.resize(100);
+	}
+	void push(const T& value)
+	{
+		// [][][front][][][][][back][][][]
+		// [fromt][][][][][back][][][][][]
+		// TODO : 다찼는지 체크
+		if (_size == _container.size())
+		{
+			// 증설 작업
+			int newSize = max(1, _size * 2); // _size가 0이면 최소 1은 반환해줌
+			vector<T> newData;
+			newData.resize(newSize);
+
+			// 데이터 복사
+			for (int i = 0; i < _size; i++)
+			{
+				int index = (_front + i) % _container.size();
+				newData[i] = _container[index];
+			}
+
+			_container.swap(newData);
+			_front = 0;
+			_back = _size;
+		}
+		_container[_back] = value;
+		_back = (_back + 1) % _container.size(); // 여기서의 사이즈는 capacity 같은 개념
+		_size++;
+	}
+
+	void pop()
+	{
+		_front = (_front + 1) % _container.size();
+		_size--;
+	}
+
+	T& front()
+	{
+		return _container[_front];
+	}
+
+	bool empty() { return _size == 0; }
+	int size() { return _size; }
+
+private:
+	vector<T> _container;
+
+	int _front = 0;
+	int _back = 0;
+	int _size = 0;
+};
+
+template<typename T>
+class ListQueue
+{
+public:
+	void push(const T& value)
+	{
+		_container.push_back(value);
+	}
+
+	void pop()
+	{
+		_container.pop_front();
+	}
+
+	T& front()
+	{
+		return _container.front();
+	}
+
+	bool empty() { return _container.empty(); }
+	int size() { return _container.size(); }
+
+private:
+	list<T> _container;
+};
+
 int main()
 {
 	//  ------------- 벡터 ---------------
@@ -330,7 +416,7 @@ int main()
 
 	*/
 
-	//  ------------- 리스트 ---------------
+	//  ------------ 리스트 --------------
 	/*
 	List<int> li;
 	List<int>::iterator eraseIt;
@@ -358,6 +444,7 @@ int main()
 	*/
 
 	//  ------------- 스택 ---------------
+	/*
 	Stack<int, list<int>> s;
 
 	// 삽입
@@ -377,4 +464,23 @@ int main()
 	}
 
 	int size = s.size();
+	*/
+
+	//  -------------- 큐 ----------------
+	/**/
+	ArrayQueue<int> q;
+
+	for (int i = 0; i < 100; i++)
+	{
+		q.push(i);
+	}
+
+	while (q.empty() == false)
+	{
+		int value = q.front();
+		q.pop();
+		cout << value << endl;
+	}
+
+	int size = q.size();
 } 
