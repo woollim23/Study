29d527b7df80e651e1089ef6cca4fd26a722ebdc
woollim <137277221+woollim23@users.noreply.github.com>
2024-09-21 00:10:09 +0900

    [level 0] Title: 문자열 출력하기, Time: 24.57 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String str;
+
+        Console.Clear();
+        str = Console.ReadLine();
+
+        Console.WriteLine(str);
+    }
+}
