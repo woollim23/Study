0f3f3618e9e1bdf04e930849d72a42ad86df2f2d
woollim23 <begin2125@gmail.com>
2024-09-23 17:35:22 +0900

    [C#기초강의] 4-3강. 델리게이트, 람다 및 LINQ

---
+++ Study_C#_Class/Class4_2/Class4_3.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class4_3
+{
+    internal class Class4_3
+    {
+        // 델리게이트 예제
+        delegate void MyDelegate(string message);
+
+        static void Method1(string message)
+        {
+            Console.WriteLine("Method1: " + message);
+        }
+
+        static void Method2(string message)
+        {
+            Console.WriteLine("Method2: " + message);
+        }
+
+        // 펑크, 액션 예제 
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static void Main(string[] args)
+        {
+            // 델리게이트 예제
+            MyDelegate myDelegate = Method1;
+            myDelegate += Method2;
+
+            myDelegate("Hello!");
+
+            // 펑크, 액션 예제
+            Func<int, int, int> addFunc = Add;
+            Console.WriteLine(addFunc(3, 5)); // 8
+
+            Action<string> printAction = PrintMessage;
+            printAction("Hello, World!"); // Hello, World!
+        }
+    }
+}
