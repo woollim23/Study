185cc57a45d072aa37e2cbc8d8ebdebcdf5129b3
woollim <137277221+woollim23@users.noreply.github.com>
2024-10-18 15:23:59 +0900

    [level 0] Title: n의 배수, Time: 0.24 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/181937. n의 배수/n의 배수.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int num, int n) {
+        int answer = 0;
+        
+        if(num % n == 0)
+            answer = 1;
+        else
+            answer = 0;
+        return answer;
+    }
+}
